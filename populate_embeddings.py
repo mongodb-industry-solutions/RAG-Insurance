ea738cd060c3de3d1e6c599bdb2875697d240fed
LucaNapoli <luc.napoli@gmail.com>
2024-02-14 14:39:57 +0100

    ok

--- populate_embeddings.py
+++ populate_embeddings.py
@@ -1,15 +1,38 @@
 from langchain_openai import OpenAIEmbeddings
-#import gradio as gr
-#from gradio.themes.base import Base
-#import key_param
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 from dotenv import load_dotenv
 import os
-import openai
+#import openai
 load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
+uri = "mongodb+srv://luca:abba@ist-shared.n0kts.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+""" try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e) """
+
+db = client['demo_rag_insurance']
+collection = db['claims_final']
+
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=2048)
-text = "This is a test document."
-query_result = embeddings.embed_query(text)
-print(len(query_result))
+########################################################################################
+
+for document in collection.find():
+    
+    claimDescription = document.get('claimDescription')
+    claimDescriptionEmbedding = embeddings.embed_query(claimDescription)
+    collection.update_one({'_id': document['_id']}, {'$set': {'claimDescriptionEmbedding': claimDescriptionEmbedding}})
+
+    damageDescription = document.get('damageDescription')
+    damageDescriptionEmbedding = embeddings.embed_query(damageDescription)
+    collection.update_one({'_id': document['_id']}, {'$set': {'damageDescriptionEmbedding': damageDescriptionEmbedding}})
+
+    print("Document " + str(document["_id"]) + " updated.")
+
+print("Embeddings created.")
