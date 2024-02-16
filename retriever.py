a9214eb9a9dd533ef24e502ad40de90837fbad67
LucaNapoli <luc.napoli@gmail.com>
2024-02-16 12:33:09 +0100

    yeeee

--- retriever.py
+++ retriever.py
@@ -20,29 +20,45 @@ CONNECTION_STRING = str(mongo_uri)
 DB_NAME = "demo_rag_insurance"
 COLLECTION_NAME = "claims_final"
 INDEX_NAME = "vector_index_claim_description"
-#INDEX_NAME = "default"
-
 
 MongoClient = MongoClient(CONNECTION_STRING)
 collection = MongoClient[DB_NAME][COLLECTION_NAME]
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small", dimensions=350, disallowed_special=())
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small", dimensions=350)
 #embeddings = OpenAIEmbeddings(model="text-embedding-ada-002-v2", dimensions=350)
 
 vector_search = MongoDBAtlasVectorSearch.from_connection_string(
     CONNECTION_STRING,
     DB_NAME + "." + COLLECTION_NAME,
-    embeddings,
+    embedding=embeddings,
     index_name=INDEX_NAME,
-    text_key="claimDescription",
-    embedding_key="claimDescriptionEmbedding"
 )
 
-query = "I had an accident because of a flat tire."
+qa_retriever = vector_search.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 5},
+)
 
-results = vector_search.similarity_search(
-    query=query, k=5)
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-# Display results
-for result in results:
-    print(result)
+{context}
+
+Question: {question}
+"""
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
+qa = RetrievalQA.from_chain_type(
+    llm=OpenAI(model="text-davinci-003"),
+    chain_type="stuff",
+    retriever=qa_retriever,
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT},
+)
+
+docs = qa({"query": "gpt-4 compute requirements"})
+
+print(docs["result"])
+print(docs["source_documents"])
