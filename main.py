7ed2d1cdaa00c8b252bf78558e49df277d3abc48
LucaNapoli <luc.napoli@gmail.com>
2024-03-06 12:14:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import APIRouter
 from ask_llm import interrogate_llm
+from image_search import image_search
 
 app = FastAPI()
 
@@ -28,3 +29,10 @@ async def ask_llm(request: Request):
     question = data.get("question")
     similar_docs, llm_output = interrogate_llm(question)
     return {"question": question, "result": llm_output, "similar_docs": similar_docs}
+
+@app.post("/imageSearch")
+async def find_similar_images(request: Request):
+    data = await request.json()
+    droppedImage = data.get("droppedImage") 
+    similar_photos = image_search(droppedImage)
+    return {"similar_photos": similar_photos}
