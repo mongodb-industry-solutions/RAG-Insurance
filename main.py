6f3287222872a771916c41256b8ae5ebbf2bfd9b
bereilhp <bereilhp@gmail.com>
2024-03-07 16:06:53 +0100

    Dockerfile and add 127.0.0.1

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://localhost:8000"
+    "http://localhost:8000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000"
 ]
 
 app.add_middleware(
